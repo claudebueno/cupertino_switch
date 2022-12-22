be64af4b5b9ba296be3e1def4ad654409d12e9a5
Claude BUENO <hello@bueno.fr>
2022-12-22 20:10:50 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      // hide the debug banner
+      debugShowCheckedModeBanner: false,
+      title: "Cupertino Switch",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showGreenBox = false;
+
+  bool _showBlueBox = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text("Cupertino Switch"),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Afficher la boite verte'),
+                CupertinoSwitch(
+                  value: _showGreenBox,
+                  onChanged: (value) {
+                    setState(() {
+                      _showGreenBox = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Afficher la boite verte'),
+                // this switch controls the existence of the blue box
+                CupertinoSwitch(
+                  activeColor: CupertinoColors.activeBlue,
+                  trackColor: CupertinoColors.inactiveGray,
+                  thumbColor: CupertinoColors.white,
+                  value: _showBlueBox,
+                  onChanged: (value) {
+                    setState(() {
+                      _showBlueBox = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+
+            // The Green Box
+            if (_showGreenBox)
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: CupertinoColors.activeGreen,
+              ),
+
+            // The Blue Box
+            if (_showBlueBox)
+              Container(
+                margin: const EdgeInsets.only(top: 30),
+                width: double.infinity,
+                height: 200,
+                color: CupertinoColors.activeBlue,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
